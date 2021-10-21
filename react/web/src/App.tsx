d4eb3cb72c757a6c78691dbcfae33b6e34f0745d
Vitor Honna <vitorhonna@gmail.com>
2021-10-21 16:19:32 -0300

    stage2 26min51

--- react/web/src/App.tsx
+++ react/web/src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css';
+import styles from './App.module.scss';
 
 export function App() {
   return (
-    <main className="contentWrapper">
+    <main className={styles.contentWrapper}>
       <h1>Hello World</h1>
     </main>
   )
