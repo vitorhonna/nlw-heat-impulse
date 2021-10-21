165791e00015103427d97a1dbbe2d1bc994b5216
Vitor <vitorhonna@gmail.com>
2021-10-20 21:29:22 -0300

    prisma client corrigido

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 /** Passos:
  * Receber code(string) do front
@@ -47,9 +48,39 @@ class AuthenticateUserService {
 
     const { login, id, avatar_url, name } = response.data
 
-    const user = await prismaClient.
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
 
-    return response.data;
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id
+        }
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d"
+      }
+    )
+
+    return { token, user };
   }
 }
 
--- updates.md
+++ updates.md
@@ -4,5 +4,7 @@ $ yarn add axios
 $ yarn add @types/axios -D
 $ yarn add @types/jsonwebtoken -D
 
-$ yarn prisma migrate dev (????)
+<!-- $ yarn prisma migrate reset -->
 $ yarn add @prisma/client
+$ yarn prisma migrate dev
+
