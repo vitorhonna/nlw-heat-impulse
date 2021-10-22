0e177bb7600146a501025f4bf26f44fb47381bed
Vitor Honna <vitorhonna@gmail.com>
2021-10-22 03:36:09 -0300

    stage 2: react finalizado

--- react/web/src/components/MessageList/index.tsx
+++ react/web/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client'
 import { api } from '../../services/api'
 
 import styles from './styles.module.scss';
@@ -14,8 +15,30 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(previousState => [
+          messagesQueue[0],
+          previousState[0],
+          previousState[1],
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
