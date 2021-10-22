d190b0ca6c0fa9a9d1f7f40e679de14ff2e372d9
Vitor Honna <vitorhonna@gmail.com>
2021-10-22 03:43:55 -0300

    Revert "Web"

--- react/web/src/App.tsx
+++ react/web/src/App.tsx
@@ -1,18 +1,12 @@
-import { useContext } from 'react';
 import styles from './App.module.scss';
-import { LoginBox } from './components/LoginBox';
-import { MessageList } from './components/MessageList';
-import { SendMessageForm } from './components/SendMessageForm';
-import { AuthContext } from './contexts/auth';
 
 export function App() {
-  const { user } = useContext(AuthContext);
-
-
   return (
-    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
-      <MessageList />
-      {!!user ? <SendMessageForm /> : <LoginBox />}
+    <main className={styles.contentWrapper}>
+      <h1>Hello World</h1>
     </main>
   )
 }
+
+
+
--- react/web/src/main.tsx
+++ react/web/src/main.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
-import { AuthProvider } from './contexts/auth'
 
 import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
