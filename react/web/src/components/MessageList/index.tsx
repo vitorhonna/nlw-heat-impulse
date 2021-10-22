6212bdb57c1b4588b71e591618616e954d35a898
Vitor Honna <vitorhonna@gmail.com>
2021-10-22 01:10:31 -0300

    últimas 3 mensagens na tela

--- react/web/src/components/MessageList/index.tsx
+++ react/web/src/components/MessageList/index.tsx
@@ -14,7 +14,7 @@ type Message = {
   }
 }
 
-export function MessageList(){
+export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
 
   useEffect(() => {
@@ -24,40 +24,24 @@ export function MessageList(){
   }, [])
 
   return (
-   <div className={styles.messageListWrapper}>
-     <img src={logoImg} alt="DoWhile 2021" />
-     
-     <ul className={styles.messageList}>
-       <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-       </li>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
 
-       <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-       </li>
-
-       <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-       </li>
-     </ul>
-   </div>
+      <ul className={styles.messageList}>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   )
 }
